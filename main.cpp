fdbe6b3d0a279b1f81efbd08699c39b900dddf5e
idsyr <idsyr@mail.ru>
2024-01-12 01:49:07 +0500

    camera 2

--- main.cpp
+++ main.cpp
@@ -11,10 +11,59 @@
 using std::cout; using std::endl; using std::sin;
 int success; char infoLog[512];
 
+uint scr_w = 1360;
+uint scr_h = 768;
+
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+
+float yaw = -90.0f;
+float pitch = -90.0f;
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+float fov = 42.0f;
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){
+    if(firstMouse){
+        lastX = xpos; lastY = ypos; firstMouse = false;}
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos; lastY = ypos;
+    
+    const float sens = 0.01f;
+    xoffset *= sens; yoffset *= sens;
+    yaw += xoffset; pitch += yoffset;
+    if(pitch>89.0f)  pitch = 89.0f;
+    if(pitch<-89.0f) pitch = -89.0f;
+
+    glm::vec3 dir; 
+    dir.x = cos(glm::radians(yaw))*cos(glm::radians(pitch));
+    dir.y = sin(glm::radians(pitch));
+    dir.z = sin(glm::radians(yaw))* cos(glm::radians(pitch));
+    cameraFront = glm::normalize(dir);
+}
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+    fov -=(float)yoffset;
+    if(fov<1.0f) fov = 1.0f;
+    if(fov>45.0f) fov = 45.0f;
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int w, int h){
     glViewport(0,0,w,h);}
 
 void processInput(GLFWwindow* window){
+    const float cameraSpeed = 2.5f*deltaTime;
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) cameraPos+=cameraSpeed*cameraFront;
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) cameraPos-=cameraSpeed*cameraFront;
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) cameraPos-=glm::normalize(glm::cross(cameraFront, cameraUp))*cameraSpeed;
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) cameraPos+=glm::normalize(glm::cross(cameraFront, cameraUp))*cameraSpeed;
+
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE)==GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);}
 
@@ -24,7 +73,7 @@ int main(){
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
-	GLFWwindow* window = glfwCreateWindow(800, 600, "experimental", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(scr_w, scr_h, "experimental", NULL, NULL);
 	if(window == NULL){cout<<"window == NULL"<<endl; glfwTerminate(); return -1;}
 	glfwMakeContextCurrent(window);
 
@@ -150,47 +199,28 @@ glm::vec3 cubePositions[] = {
     shaderProgram.setInt("texture2", 1);
 
     glEnable(GL_DEPTH_TEST);
-
-    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
     while(!glfwWindowShouldClose(window)){
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
-		processInput(window);
-		glClearColor(0.5f, 0.4f, 0.7f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        processInput(window);
+        glClearColor(0.5f, 0.4f, 0.7f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
-        //glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glm::mat4 trans = glm::mat4(1.0f);
-        //trans = glm::translate(trans, glm::vec3(-0.2f, 0.2f, 0.0f));
-        //trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        //uint transformLoc = glGetUniformLocation(shaderProgram.shaderProgram, "transform");
-        //glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-         
-
-        //glUniform4f(glGetUniformLocation(shaderProgram, "ourColor"), 
-        //            0.1f, sin(glfwGetTime()/2.0f+0.5f), 0.3f, 1.0f);
         
-        glm::vec3 cameraDirection = glm::normalize(cameraPos-cameraTarget);
-        glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
-        glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
-        const float radius = 10.0f;
-        float camX = sin(glfwGetTime())*radius;
-        float camZ = cos(glfwGetTime())*radius;
-        glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0f, camZ),
-                                     glm::vec3(0.0f, 0.0f, 0.0f),
-                                     glm::vec3(0.0f, 1.0f, 0.0f));
-        //glm::mat4 view = glm::mat4(1.0f);
-        //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(fov), (float)scr_w / (float)scr_h, 0.1f, 100.0f);
         
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram.shaderProgram, "view"),
                            1, GL_FALSE, glm::value_ptr(view));
@@ -209,14 +239,9 @@ glm::vec3 cubePositions[] = {
         
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-
-
         
-        
-        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 	}
 
     glDeleteVertexArrays(1, &VAO);
